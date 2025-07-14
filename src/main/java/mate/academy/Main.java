c9365fea8a8eefa24691a432ce3588ff670ea7f2
Biletska Liudmyla <bileckaludmila74@gmail.com>
2025-07-14 15:58:25 +0200

    modific Main.java

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -3,8 +3,7 @@ package mate.academy;
 import mate.academy.service.AuthenticationService;
 
 public class Main {
-    private static final AuthenticationService authenticationService =
-            new AuthenticationService(new UserService());
+    private static final AuthenticationService authenticationService = new AuthenticationService();
 
     public static void main(String[] args) {
         test("bob@i.ua", "1234", true);
